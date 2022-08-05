c6d441f1afb175da4072e1a4595f09e539cec0ed
Sakda898 <Sakda898@gmail.com>
2022-08-05 16:19:10 +0700

    Create New Models In Odoo & Create Menu And Action

--- hospital/__manifest__.py
+++ hospital/__manifest__.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "deposit",
+    'name': "Hospital Management",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        ระบบทดสอบการเรียนรู้การสร้าง  Module ในระบบ Odoo
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Mr.Sakda Taenmanee",
+    'website': "http://www.Sakda-Taenmanee.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -25,11 +25,14 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/patient.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
